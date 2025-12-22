ddee880aae999d17d53ad9631b33334fd217fa75
rudra-webdev <rudrapratapmishra60@gmail.com>
2025-12-22 20:35:44 +0530

    Cloud storage,PDF-Image conversion,Key-Value storage and AI feedback

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     }, [auth.isAuthenticated]);
 
 
-  return <main className="bg-[url('/images/bg-main.svg/')] bg-cover">
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar/>
 
       <section className="main-section">
